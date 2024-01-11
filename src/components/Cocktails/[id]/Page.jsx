a43ce90978c2be93a15784aca572fa91d3b2fb2f
abdisalomoff <Avazbek6757@gmail.com>
2024-01-11 05:00:11 +0500

    add

--- src/components/Cocktails/[id]/Page.jsx
+++ src/components/Cocktails/[id]/Page.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+import useFetch from "@/hooks/useFetch";
+import { useRouter } from "next/router";
 
-const page = () => {
+const CocktailsSingle = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { drink } = useFetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+
+  console.log(drink);
+
+  if (!drink) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h2>{drink.strDrink}</h2>
+      <p>{drink.strInstructions}</p>
+    </div>
+  );
+};
 
-export default page
+export default CocktailsSingle;
